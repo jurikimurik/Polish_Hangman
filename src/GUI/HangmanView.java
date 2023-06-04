62cd9a96517e6ab81232e977690f31fda6b51218
Jurikimurik <makovskyiyurii@gmail.com>
2023-06-04 11:22:14 +0200

    Example GUI was created.

--- src/GUI/HangmanView.java
+++ src/GUI/HangmanView.java
@@ -3,4 +3,11 @@ package GUI;
 import javax.swing.*;
 
 public class HangmanView extends JFrame {
+    private JPanel panel1;
+    private JTextField currentWordField;
+    private JTextField descriptionField;
+    private JLabel imageLabel;
+    private JLettersPanel lettersPanel;
+    private JLabel wordLabel;
+    private JLabel descriptionLabel;
 }
