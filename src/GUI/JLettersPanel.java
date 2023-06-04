522659a09d92ea73b3f5b7e4282e4a19edefaac3
Jurikimurik <makovskyiyurii@gmail.com>
2023-06-04 11:40:54 +0200

    Panel with letters has been created.

--- src/GUI/JLettersPanel.java
+++ src/GUI/JLettersPanel.java
@@ -1,12 +1,30 @@
 package GUI;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class JLettersPanel extends JPanel implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
+public class JLettersPanel extends JPanel {
+    String[] buttonNames = {"A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "V", "X", "Y", "Z", "Ą", "Ę", "Ł", "Ó", "Ż"};
 
+    ArrayList<JButton> buttons;
+
+    public JLettersPanel() {
+        FlowLayout layout = new FlowLayout();
+        setLayout(layout);
+
+        buttons = new ArrayList<>();
+        for(String text : buttonNames) {
+            buttons.add(new JButton(text));
+        }
     }
+
+    public void setListenerForButtons(ActionListener listener) {
+        for(JButton button : buttons)
+            button.addActionListener(listener);
+    }
+
 }
